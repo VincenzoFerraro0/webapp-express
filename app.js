1adc5d9b016865e20b3672ab54cd9163dd6fb5dd
VincenzoFerraro0 <ferrarov0@gmail.com>
2025-03-14 18:02:53 +0100

    I update the app.js file

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 import express from 'express';
+import movieRouter from './routers/movieRouter.js'; // Importiamo il router per le rotte dei film
 
-const app = express(); // Creiamo un'istanza di un'applicazione Express
+const app = express(); // Creiamo un'istanza dell'applicazione Express
 
 // Definiamo la porta su cui il server ascolterà, prendendola dall'ambiente o impostandola su 3000
 const port = process.env.SERVER_PORT || 3000;
 
-// Avviamo il server e facciamo in modo che ascolti sulla porta definita
+// Middleware per instradare tutte le richieste a /movies verso movieRouter
+app.use('/movies', movieRouter);
+
+// Avviamo il server e lo mettiamo in ascolto sulla porta specificata
 app.listen(port, () => {
     console.log(`Server Movies in funzione sulla porta: ${port}`);
 });
